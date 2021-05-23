2ad8043ec89b4d87667046283e15cfd560f46b9a
kumud-shiprocket <kumud.gupta@kartrocket.com>
2021-05-23 12:59:38 +0530

    Intial commit

---
+++ src/round/c/Problem1.java
@@ -0,0 +1,15 @@
+package round.c;
+
+import java.util.Scanner;
+
+public class Problem1 {
+    public static  void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int t =sc.nextInt();
+        int I = 0;
+        while(t-- > 0) {
+            System.out.println("Case #"+I+": ");
+        }
+    }
+}
